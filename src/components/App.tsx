5ff0377e291927184a8b8d72df62b17e31744695
Derui DENG <moonface.ddr@gmail.com>
2018-08-16 22:47:30 +0800

    Setup basic functionality of react-router.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,32 @@
 import * as React from "react";
-import { hot } from 'react-hot-loader';
-
-import { Hello } from "./helloworld/Hello";
+import {hot} from 'react-hot-loader';
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
+import {Hello} from "./helloworld/Hello";
 import {SimpleMessage} from "./SimpleMessage/SimpleMessage";
+import Catalog from "./catalog/Catalog";
 
-const App = () => (
-    <div>
-        <Hello name="Typescript" />
-        <SimpleMessage title={"Simple Message title"} description="This is description."/>
-    </div>
-);
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Catalog />
+                <Switch>
+                    <Route
+                        exact
+                        path='/'
+                        component={Hello}
+                    />
+                    <Route
+                        path='/message'
+                        render={() => <SimpleMessage
+                            title={"Title of simple message"}
+                            description={"Description blablablabla"}
+                        />}
+                    />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
+};
 
 export default hot(module)(App);
