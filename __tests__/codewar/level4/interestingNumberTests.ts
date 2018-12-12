c9b26ecf1139c699a018fa9778be5b5901867136
Derui DENG <moonface.ddr@gmail.com>
2018-12-12 21:50:02 +0800

    Add codewar answer.

---
+++ __tests__/codewar/level4/interestingNumberTests.ts
@@ -0,0 +1,59 @@
+import { Kata } from '../../../src/codewar/level4/interestingNumber';
+
+describe('IsExactlyInteresting', function () {
+  it('Should check digit followed by all zeros', function () {
+    expect(Kata.isExactlyInteresting(1000, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(100, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(9001, [])).toBe(false);
+    expect(Kata.isExactlyInteresting(1200, [])).toBe(false);
+  });
+
+  it('Should check number shorter than 3 digits', function () {
+    expect(Kata.isExactlyInteresting(90, [])).toBe(false);
+    expect(Kata.isExactlyInteresting(11, [])).toBe(false);
+  });
+
+  it('Should check same digits', function () {
+    expect(Kata.isExactlyInteresting(111, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(99999, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(1112, [])).toBe(false);
+    expect(Kata.isExactlyInteresting(2111, [])).toBe(false);
+  });
+
+  it('Should check increment sequential digits', function () {
+    expect(Kata.isExactlyInteresting(123, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(890, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(5678, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(901, [])).toBe(false);
+  });
+
+  it('Should check decrement sequential digits', function () {
+    expect(Kata.isExactlyInteresting(321, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(210, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(87654, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(109, [])).toBe(false);
+  });
+
+  it('Should check palindrome number', function () {
+    expect(Kata.isExactlyInteresting(131, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(13531, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(2442, [])).toBe(true);
+    expect(Kata.isExactlyInteresting(24544, [])).toBe(false);
+    expect(Kata.isExactlyInteresting(33533, [])).toBe(true);
+  });
+
+  it('Should check awesomePhrases', function () {
+    expect(Kata.isExactlyInteresting(134, [111,112,113])).toBe(false);
+    expect(Kata.isExactlyInteresting(134, [111,112,134])).toBe(true);
+    expect(Kata.isExactlyInteresting(13531, [135])).toBe(true);
+  });
+});
+
+describe('IsInteresting', function () {
+  it('Should check digit followed by all zeros', function () {
+    expect(Kata.isInteresting(1000, [])).toBe(2);
+    expect(Kata.isInteresting(1002, [])).toBe(1);
+    expect(Kata.isInteresting(998, [])).toBe(1);
+    expect(Kata.isInteresting(1453, [])).toBe(0);
+  });
+});
---
+++ src/codewar/level4/interestingNumber.ts
@@ -0,0 +1,80 @@
+/*
+"Interesting" Numbers
+Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
+
+  Any digit followed by all zeros: 100, 90000
+Every digit is the same number: 1111
+The digits are sequential, incementing†: 1234
+The digits are sequential, decrementing‡: 4321
+The digits are a palindrome: 1221 or 73837
+The digits match one of the values in the awesomePhrases array
+† For incrementing sequences, 0 should come after 9, and not before 1, as in 7890.
+‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
+*/
+
+const isFollowByAllZeros = (num: number): boolean => {
+  return /^\d[0]+$/.test(num.toString());
+};
+
+const isSameNumber = (num: number): boolean => {
+  const numString = num.toString();
+  return numString === numString.charAt(0).repeat(numString.length);
+};
+
+const isIncrementSequential = (num: number): boolean => {
+  const digits = num.toString().split('').map(digitString => parseInt(digitString));
+  for (let i = 1; i < digits.length; i++) {
+    const digit = digits[i];
+    const prevDigit = digits[i - 1];
+    if (prevDigit === 0 || (digit - prevDigit !== 1 && digit - prevDigit !== -9)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isDecrementSequential = (num: number): boolean => {
+  const digits = num.toString().split('').map(digitString => parseInt(digitString));
+  for (let i = 1; i < digits.length; i++) {
+    const digit = digits[i];
+    const prevDigit = digits[i - 1];
+    if (prevDigit === 0 || (digit - prevDigit !== -1)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const isPalindrome = (num: number): boolean => {
+  const digits = num.toString().split('').map(digitString => parseInt(digitString));
+  return digits.every((digit, index) => digit === digits[digits.length - 1 - index]);
+};
+
+const isMatchingAwesomePhrases = (num: number, awesomePhrases: number[]): boolean => {
+  return awesomePhrases.some(phrase => phrase === num);
+};
+
+const isNumberValid = (num: number): boolean => {
+  return Number.isInteger(num) && num >= 100;
+};
+
+export class Kata {
+  static isInteresting(n: number, awesomePhrases: number[]): number {
+    if (Kata.isExactlyInteresting(n, awesomePhrases)) {
+      return 2
+    }
+
+    if ([n + 1, n + 2].some(n => Kata.isExactlyInteresting(n, awesomePhrases))) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  static isExactlyInteresting(n: number, awesomePhrases: number[]): boolean {
+    return isNumberValid(n) && [isFollowByAllZeros, isSameNumber, isIncrementSequential, isDecrementSequential, isPalindrome, isMatchingAwesomePhrases].some(
+      fn => fn(n, awesomePhrases) === true);
+  }
+}
