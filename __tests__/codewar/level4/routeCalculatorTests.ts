335cfd6546239eacfc1d3624b7f6272a2d151f7e
Derui DENG <moonface.ddr@gmail.com>
2018-12-13 23:05:04 +0800

    Add codewar answers.

---
+++ __tests__/codewar/level4/routeCalculatorTests.ts
@@ -0,0 +1,43 @@
+import { calculate } from '../../../src/codewar/level4/routeCalculator';
+
+describe('operate', function () {
+  it('should return a valid single input number', function () {
+    expect(calculate('1')).toBe(1);
+    expect(calculate('1.1')).toBe(1.1);
+  });
+
+  it('should perform simple single operations', function () {
+    expect(calculate('1+1')).toBe(2);
+    expect(calculate('1-1')).toBe(0);
+    expect(calculate('2$2')).toBe(1);
+    expect(calculate('2*2')).toBe(4);
+  });
+
+  it('should work with decimal numbers', function () {
+    expect(calculate('1.1+1.9')).toBe(3);
+    expect(calculate('9$4')).toBe(2.25);
+    expect(calculate('1.5*3')).toBe(4.5);
+    expect(calculate('5-43.2')).toBe(-38.2);
+  });
+
+  it('should accept many of same operator', function () {
+    expect(calculate('5+5+5+5')).toBe(20);
+    expect(calculate('5-5-5-5')).toBe(-10);
+    expect(calculate('5*5*5*5')).toBe(625);
+    expect(calculate('5$5$5$5')).toBe(0.04);
+  });
+
+  it('should calculate everything thrown at it', function () {
+    expect(calculate('1+1-1')).toBe(1);
+    expect(calculate('5*6$2+5-10')).toBe(10);
+    expect(calculate('1*1*1*1*1*1$1$1$1$1+1-1+9-1')).toBe(9);
+    expect(calculate('1000$2.5$5+5-5+6$6')).toBe(81);
+  });
+
+  it('should throw input error for bad inputs', function () {
+    expect(calculate('5*6$2&5-10')).toBe('400: Bad request');
+    expect(calculate('5/10')).toBe('400: Bad request');
+    expect(calculate('p')).toBe('400: Bad request');
+    expect(calculate('9^9')).toBe('400: Bad request');
+  });
+});
---
+++ src/codewar/level4/matrixDeterminant.ts
@@ -0,0 +1,27 @@
+/**
+ * Matrix Determinant
+ * https://www.codewars.com/kata/matrix-determinant/train/typescript
+ */
+
+export function determinant(m: Array<number[]>): number {
+  if (m.length === 1) {
+    return m[0][0];
+  }
+
+  if (m.length === 2) {
+    return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+  }
+
+  let total = 0;
+  for (let columnIndex = 0; columnIndex < m[0].length; columnIndex++) {
+    total += determinant(getSubMatrix(m, columnIndex))
+      * m[0][columnIndex] * (columnIndex % 2 * (-2) + 1);
+  }
+
+  return total;
+}
+
+export function getSubMatrix(m: Array<number[]>, column: number): Array<number[]> {
+  return m.filter((row, index) => index !== 0)
+    .map(row => row.filter((element, index) => index !== column));
+}
