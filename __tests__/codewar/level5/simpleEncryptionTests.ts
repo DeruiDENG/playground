84b9828580c5f4075a81f1e05cad8f774193e809
DeruiDENG <drdeng@thoughtworks.com>
2018-12-14 23:47:30 +0800

    Add new codewar answer.

---
+++ __tests__/codewar/level5/simpleEncryptionTests.ts
@@ -0,0 +1,28 @@
+import {assert} from "chai";
+import { decrypt, encrypt } from '../../../src/codewar/level5/simpleEncryption';
+
+describe("solution", function(){
+  it("EncryptExampleTests", function(){
+    assert.equal(encrypt("A", 111), "S");
+    assert.equal(encrypt("Abc", 212), "Smb");
+    assert.equal(encrypt("Wave", 0), "Wave"); // -> 000
+    assert.equal(encrypt("Wave", 345), "Tg.y");
+    assert.equal(encrypt("Ball", 134), ">fdd");
+    assert.equal(encrypt("Ball", 444), ">gff");
+
+    assert.equal(encrypt("This is a test.", 348), "Iaqh qh g iyhi,");
+    assert.equal(encrypt("Do the kata Kobayashi Maru Test. Endless fun and excitement when finding a solution.", 583), "Sr pgi jlpl Jr,lqlage Zlow Piapc I.skiaa dw. l.s ibnepizi.p ugi. de.se.f l arkwper.c");
+  });
+
+  it("DecryptExampleTests", function(){
+    assert.equal(decrypt("S", 111), "A");
+    assert.equal(decrypt("Smb", 212), "Abc");
+    assert.equal(decrypt("Wave", 0), "Wave"); // -> 000
+    assert.equal(decrypt("Tg.y", 345), "Wave");
+    assert.equal(decrypt(">fdd", 134), "Ball");
+    assert.equal(decrypt(">gff", 444), "Ball");
+
+    assert.equal(decrypt("Iaqh qh g iyhi,", 348), "This is a test.");
+    assert.equal(decrypt("Sr pgi jlpl Jr,lqlage Zlow Piapc I.skiaa dw. l.s ibnepizi.p ugi. de.se.f l arkwper.c", 583), "Do the kata Kobayashi Maru Test. Endless fun and excitement when finding a solution.");
+  });
+});
---
+++ src/codewar/level5/simpleEncryption.ts
@@ -0,0 +1,80 @@
+/***
+ * Simple Encryption #4 - Qwerty
+ * https://www.codewars.com/kata/57f14afa5f2f226d7d0000f4/train/typescript
+ */
+enum Categary {
+  FirstLine,
+  SecondLine,
+  ThirdLine,
+}
+
+type KeyMap = { type: Categary, chars: string[] }
+const keyMaps: KeyMap[] = [
+  {
+    type: Categary.FirstLine,
+    chars: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  },
+  {
+    type: Categary.FirstLine,
+    chars: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+  }, {
+    type: Categary.SecondLine,
+    chars: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  }, {
+    type: Categary.SecondLine,
+    chars: ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+  }, {
+    type: Categary.ThirdLine,
+    chars: ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.'],
+  }, {
+    type: Categary.ThirdLine,
+    chars: ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>'],
+  }
+];
+
+type EncryptionKeys = [
+  { type: Categary.FirstLine, value: number },
+  { type: Categary.SecondLine, value: number },
+  { type: Categary.ThirdLine, value: number }
+  ]
+
+export function encrypt(text: string, key: number): string {
+  const parsedKeys = parseEncryptionKey(key);
+  return text.split('').map(char => processChar(char, parsedKeys, true)).join('');
+}
+
+export function decrypt(text: string, key: number): string {
+  const parsedKeys = parseEncryptionKey(key);
+  return text.split('').map(char => processChar(char, parsedKeys, false)).join('');
+}
+
+function parseEncryptionKey(key: number): EncryptionKeys {
+  const keyDigits = `00${key.toString()}`.split('').slice(-3).map(str => parseInt(str, 10));
+  return [
+    { type: Categary.FirstLine, value: keyDigits[0] },
+    { type: Categary.SecondLine, value: keyDigits[1] },
+    { type: Categary.ThirdLine, value: keyDigits[2] }];
+}
+
+function processChar(char: string, keys: EncryptionKeys, isEncrypt: boolean) {
+  const keyMap = findKeyMap(char);
+  if (!keyMap) {
+    return char;
+  }
+
+  const { type, chars } = keyMap;
+  const { value } = keys.find(key => key.type === type);
+
+  return findCharInArray(char, chars, isEncrypt ? value : -value);
+}
+
+function findCharInArray(currentChar: string, chars: string[], offset: number): string {
+  const charsSize = chars.length;
+  const currentPosition = chars.findIndex(char => char === currentChar);
+  const newPosition = (currentPosition + offset) % charsSize;
+  return chars[newPosition >= 0 ? newPosition : newPosition + charsSize];
+}
+
+function findKeyMap(char: string): KeyMap {
+  return keyMaps.find(keyMap => keyMap.chars.some(charInMap => charInMap === char));
+}
