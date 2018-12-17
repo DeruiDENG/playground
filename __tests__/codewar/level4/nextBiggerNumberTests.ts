0c1eda07bdaf7dd7d152e72b09ac9aa18d49d8ba
Derui DENG <moonface.ddr@gmail.com>
2018-12-17 22:42:22 +0800

    Add codewar answers.

---
+++ __tests__/codewar/level4/nextBiggerNumberTests.ts
@@ -0,0 +1,11 @@
+import { nextBigger } from '../../../src/codewar/level4/nextBiggerNumber';
+
+describe('nextBiggerNumber', function () {
+  it('should work', function () {
+    expect(nextBigger(12)).toBe(21);
+    expect(nextBigger(513)).toBe(531);
+    expect(nextBigger(2017)).toBe(2071);
+    expect(nextBigger(414)).toBe(441);
+    expect(nextBigger(144)).toBe(414);
+  });
+});
