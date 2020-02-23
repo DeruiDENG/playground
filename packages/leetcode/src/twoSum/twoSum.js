ef795e3f1ec130d2902999b81bbe086783393c0a
deruideng <moonface.ddr@gmail.com>
2020-02-23 21:03:22 +0800

    Setup leetcode ut

--- packages/leetcode/src/twoSum/twoSum.js
+++ packages/leetcode/src/twoSum/twoSum.js
@@ -3,5 +3,4 @@
  * @param {number} target
  * @return {number[]}
  */
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-var twoSum = function(nums, target) {};
+export const twoSum = function(nums, target) {};
