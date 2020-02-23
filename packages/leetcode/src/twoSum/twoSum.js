8c98096f0f85c52be94fcd46681cb273578f6a58
deruideng <moonface.ddr@gmail.com>
2020-02-23 21:25:18 +0800

    Two sum simple implementation

--- packages/leetcode/src/twoSum/twoSum.js
+++ packages/leetcode/src/twoSum/twoSum.js
@@ -3,4 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
-export const twoSum = function(nums, target) {};
+export const twoSum = function(nums, target) {
+  for (let firstNumIndex = 0; firstNumIndex < nums.length; firstNumIndex++) {
+    const firstNum = nums[firstNumIndex];
+    for (
+      let secondNumIndex = firstNumIndex + 1;
+      secondNumIndex < nums.length;
+      secondNumIndex++
+    ) {
+      const secondNum = nums[secondNumIndex];
+      if (firstNum + secondNum === target) {
+        return [firstNumIndex, secondNumIndex];
+      }
+    }
+  }
+};
