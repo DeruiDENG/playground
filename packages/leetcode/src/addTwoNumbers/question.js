26613d6f045e7c393b8065c0edcc19718bf7eef9
deruideng <moonface.ddr@gmail.com>
2020-02-24 23:30:08 +0800

    Answer to question 2

--- packages/leetcode/src/addTwoNumbers/question.js
+++ packages/leetcode/src/addTwoNumbers/question.js
@@ -12,45 +12,30 @@
  * @return {ListNode}
  */
 export const addTwoNumbers = function(l1, l2) {
-  const num1 = listNodeToNumber(l1);
-  const num2 = listNodeToNumber(l2);
-  return numberToListNode(num1 + num2);
-};
+  /**
+   * @type {ListNode}
+   */
+  const result = {
+    val: 0,
+    next: null,
+  };
+  let ptr = result;
+  while (l1 !== null || l2 !== null) {
+    const l1Val = l1 ? l1.val : 0;
+    const l2Val = l2 ? l2.val : 0;
+    const total = ptr.val + l1Val + l2Val;
+    l1 = l1 ? l1.next : l1;
+    l2 = l2 ? l2.next : l2;
 
-/**
- *
- * @param {ListNode} listNode
- * @return {number}
- */
-function listNodeToNumber(listNode) {
-  const numbers = [];
-  while (true) {
-    numbers.push(listNode.val);
-    if (listNode.next === null) {
-      return Number.parseInt(numbers.reverse().join(''), 10);
+    ptr.val = total % 10;
+    if (total >= 10 || l1 || l2) {
+      ptr.next = {
+        val: total >= 10 ? 1 : 0,
+        next: null,
+      };
+      ptr = ptr.next;
     }
-
-    listNode = listNode.next;
-  }
-}
-
-/**
- *
- * @param {number} number
- * @return {ListNode}
- */
-function numberToListNode(number) {
-  const digits = number
-    .toString()
-    .split('')
-    .map(digit => parseInt(digit, 10));
-  let listNode = null;
-  for (const digit of digits) {
-    listNode = {
-      val: digit,
-      next: listNode,
-    };
   }
 
-  return listNode;
-}
+  return result;
+};
--- packages/leetcode/src/addTwoNumbers/question.test.js
+++ packages/leetcode/src/addTwoNumbers/question.test.js
@@ -1,29 +1,41 @@
+// @ts-check
 import { addTwoNumbers } from './question';
 
 describe('quesitons', () => {
   it('should work', () => {
-    const l1 = {
-      val: 2,
-      next: {
-        val: 4,
-        next: {
-          val: 3,
-          next: null,
-        },
-      },
-    };
-
-    const l2 = {
-      val: 5,
-      next: {
-        val: 6,
-        next: {
-          val: 4,
-          next: null,
-        },
-      },
-    };
+    const l1 = buildListNodeFromTestCase([2, 4, 3]);
+    const l2 = buildListNodeFromTestCase([5, 6, 4]);
     const result = addTwoNumbers(l1, l2);
-    expect(result).toEqual({ val: 7, next: { val: 0, next: { val: 8 } } });
+    expect(result).toEqual({
+      val: 7,
+      next: { val: 0, next: { val: 8, next: null } },
+    });
+  });
+  it('should work for long list', () => {
+    // prettier-ignore
+    const l1 = buildListNodeFromTestCase([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+    // prettier-ignore
+    const l2 = buildListNodeFromTestCase([5, 6, 4]);
+    // prettier-ignore
+    const expected = buildListNodeFromTestCase([6, 6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
+    const result = addTwoNumbers(l1, l2);
+    expect(result).toEqual(expected);
   });
 });
+
+/**
+ *
+ * @param {number[]} inputs
+ * @return {import('./question').ListNode}
+ */
+function buildListNodeFromTestCase(inputs) {
+  let nodeList = null;
+  for (const input of [...inputs].reverse()) {
+    nodeList = {
+      val: input,
+      next: nodeList,
+    };
+  }
+
+  return nodeList;
+}
