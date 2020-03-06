fe609f3e086adadf827771feb3a7aacda16ab45e
deruideng <moonface.ddr@gmail.com>
2020-03-07 00:16:50 +0800

    LC-i51 Use slide window to improve performance.

--- packages/leetcode/src/i51/i51.js
+++ packages/leetcode/src/i51/i51.js
@@ -9,24 +9,20 @@ export const findContinuousSequence = function(target) {
    * @type {number[][]}
    */
   const result = [];
-  for (let length = Math.floor(target / 2); length >= 2; length--) {
-    const isOddLength = length % 2 === 1;
-    if (isOddLength) {
-      if (target % length === 0) {
-        const valueInTheMiddle = target / length;
-        const startValue = valueInTheMiddle - (length - 1) / 2;
-        if (startValue > 0) {
-          result.push(generateArray(startValue, length));
-        }
+  const array = generateArray(1, Math.ceil(Math.sqrt(target * 2)));
+  let total = array.reduce((total, val) => total + val, 0);
+  while (array.length >= 2) {
+    if (total >= target) {
+      if (total === target) {
+        result.push([...array]);
       }
-    } else {
-      if (target % length !== 0 && (target * 2) % length === 0) {
-        const sumOfCenterTwo = (target * 2) / length;
-        const startValue = (sumOfCenterTwo + 1) / 2 - length / 2;
-        if (startValue > 0) {
-          result.push(generateArray(startValue, length));
-        }
-      }
+      const removedValue = array.pop();
+      total = total - removedValue;
+    } else if (total < target) {
+      const addedValue = array[array.length - 1] + 1;
+      array.push(array[array.length - 1] + 1);
+      const removedValue = array.shift();
+      total = total + addedValue - removedValue;
     }
   }
 
