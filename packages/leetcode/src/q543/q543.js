b01daf0774d276616fad817f215df81a12d21c38
deruideng <moonface.ddr@gmail.com>
2020-03-10 21:10:32 +0800

    LC-543 Use basic tree traversal to get longest path

--- packages/leetcode/src/q543/q543.js
+++ packages/leetcode/src/q543/q543.js
@@ -13,6 +13,7 @@
  * @prop {TreeNode} [left]
  * @prop {TreeNode} [right]
  * @prop {number} val
+ * @prop {number} [longest] - longest path passing the node as root
  */
 
 /**
@@ -20,5 +21,37 @@
  * @return {number}
  */
 export const diameterOfBinaryTree = function(root) {
-  return 0;
+  if (!root) {
+    return 0;
+  }
+  updateNodeWithLongestDistanceToNode(root);
+  return findLongestPath(root);
 };
+
+/**
+ *
+ * @param {TreeNode} node
+ * @return {number} - longest distance to leaf
+ */
+function updateNodeWithLongestDistanceToNode(node) {
+  const { left, right } = node;
+  const leftDistance = left ? updateNodeWithLongestDistanceToNode(left) + 1 : 0;
+  const rightDistance = right
+    ? updateNodeWithLongestDistanceToNode(right) + 1
+    : 0;
+  const longestPathToLeaf = Math.max(leftDistance, rightDistance);
+  node.longest = leftDistance + rightDistance;
+  return longestPathToLeaf;
+}
+
+/**
+ *
+ * @param {TreeNode} treeRoot
+ * @return {number}
+ */
+function findLongestPath(treeRoot) {
+  const leftMax = treeRoot.left ? findLongestPath(treeRoot.left) : 0;
+  const rightMax = treeRoot.right ? findLongestPath(treeRoot.right) : 0;
+  const selfMax = treeRoot.longest;
+  return Math.max(leftMax, rightMax, selfMax);
+}
