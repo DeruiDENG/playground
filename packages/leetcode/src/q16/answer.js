d0573c90116b000696b4dd917802340985ca4c13
deruideng <moonface.ddr@gmail.com>
2020-03-31 14:54:32 +0800

    LC-16 Implement using similar algorithm with Q15

--- packages/leetcode/src/q16/answer.js
+++ packages/leetcode/src/q16/answer.js
@@ -7,6 +7,33 @@
  */
 var threeSumClosest = function (nums, target) {
   nums.sort((a, b) => a - b);
+  let result = Number.MAX_SAFE_INTEGER;
+  let smallestDifference = Number.MAX_SAFE_INTEGER;
+  for (let firstNumIndex = 0; firstNumIndex < nums.length; firstNumIndex++) {
+    let secondNumberIndex = firstNumIndex + 1;
+    let thirdNumberIndex = nums.length - 1;
+    while (secondNumberIndex < thirdNumberIndex) {
+      const total =
+        nums[firstNumIndex] + nums[secondNumberIndex] + nums[thirdNumberIndex];
+      const difference = Math.abs(total - target);
+      if (difference < smallestDifference) {
+        result = total;
+        smallestDifference = difference;
+      }
+
+      if (total === target) {
+        break;
+      }
+
+      if (total < target) {
+        secondNumberIndex++;
+      } else if (total > target) {
+        thirdNumberIndex--;
+      }
+    }
+  }
+
+  return result;
 };
 
 export { threeSumClosest };
