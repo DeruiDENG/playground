68daafa2052b9a48173a37d3973c9abfbe8aedfb
Derui DENG <moonface.ddr@gmail.com>
2020-04-15 22:53:18 +0800

    Blog: Init setup based on gatsby-starter-blog

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,40 +6,48 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "plugin:react/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
-    project: "./tsconfig.json",
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ["react", "@typescript-eslint/eslint-plugin"],
+  plugins: ['react', '@typescript-eslint/eslint-plugin'],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/explicit-member-accessibility": 0,
-    "@typescript-eslint/no-use-before-define": [
-      "error",
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/no-use-before-define': [
+      'error',
       {
         functions: false,
         classes: true,
       },
     ],
-    "react/display-name": 0,
+    'react/display-name': 0,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
+  overrides: [
+    {
+      files: ['!**/src/**'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 };
---
+++ packages/blog/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rules: {
+    '@typescript-eslint/camelcase': 0,
+    'react/prop-types': 0, // Will not need this rule after typescript refactor is done.
+  },
+};
