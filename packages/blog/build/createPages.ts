a9857ce96d6943b1ba26d704401714a4ac8b589a
Derui DENG <moonface.ddr@gmail.com>
2020-04-21 23:51:20 +0800

    Blog: Add pagination for blog index, merge markdown result with wordpress result

--- packages/blog/build/createPages.ts
+++ packages/blog/build/createPages.ts
@@ -8,36 +8,121 @@ export const createPages: GatsbyNode['createPages'] = async ({
 }) => {
   const { createPage } = actions;
 
-  const blogPost = path.resolve(`./src/templates/blog-post.tsx`);
   const [markdownPosts, wordPressPages] = await Promise.all([
     getPostsFromMarkdown(graphql),
     getPostsFromWordPress(graphql),
   ]);
 
   const posts = [...markdownPosts, ...wordPressPages].sort(
-    (post1, post2) => post1.date.getTime() - post2.date.getTime()
+    (post1, post2) => post2.date.getTime() - post1.date.getTime()
   );
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1];
-    const next = index === 0 ? null : posts[index - 1];
+  createBlogPages(posts);
+  await createBlogIndexPages(posts);
 
-    createPage({
-      path: decodeURI(post.slug),
-      component: blogPost,
-      context: {
-        slug: post.slug,
-        previous,
-        next,
-      },
+  function createBlogPages(posts: Post[]) {
+    const blogPost = path.resolve(`./src/templates/blog-post.tsx`);
+    posts.forEach((post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1];
+      const next = index === 0 ? null : posts[index - 1];
+
+      createPage<PageContext>({
+        path: decodeURI(post.slug),
+        component: blogPost,
+        context: {
+          slug: post.slug,
+          previous,
+          next,
+        },
+      });
     });
-  });
+  }
+
+  async function createBlogIndexPages(posts: Post[]) {
+    const template = path.resolve(`./src/templates/BlogList.tsx`);
+    const postsPerPage = await getPostsPerPage();
+    let pageIndex = 0;
+    while (true) {
+      const postsForOnePage = posts.slice(
+        pageIndex * postsPerPage,
+        (pageIndex + 1) * postsPerPage
+      );
+
+      if (!postsForOnePage.length) {
+        break;
+      }
+
+      const mdStartIndex = postsForOnePage.find(post => post.source === 'md')
+        ?.index;
+      const mdLastIndex = postsForOnePage
+        .slice()
+        .reverse()
+        .find(post => post.source === 'md')?.index;
+      const mdCount =
+        mdStartIndex === undefined
+          ? 0
+          : (mdLastIndex as number) - mdStartIndex + 1;
+
+      const wpStartIndex = postsForOnePage.find(post => post.source === 'wp')
+        ?.index;
+      const wpLastIndex = postsForOnePage
+        .slice()
+        .reverse()
+        .find(post => post.source === 'wp')?.index;
+      const wpCount =
+        wpStartIndex === undefined
+          ? 0
+          : (wpLastIndex as number) - wpStartIndex + 1;
+
+      createPage<BlogListContext>({
+        path: pageIndex === 0 ? '/' : `/page/${pageIndex}`,
+        component: template,
+        context: {
+          pageIndex,
+          mdStartIndex: mdStartIndex === undefined ? -1 : mdStartIndex,
+          mdCount,
+          wpStartIndex: wpStartIndex === undefined ? -1 : wpStartIndex,
+          wpCount,
+        },
+      });
+
+      pageIndex++;
+    }
+  }
+
+  async function getPostsPerPage() {
+    const siteConfig = await graphql(
+      `
+        {
+          site {
+            siteMetadata {
+              blogList {
+                pagination
+              }
+            }
+          }
+        }
+      `
+    );
+
+    return (siteConfig.data as {
+      site: { siteMetadata: { blogList: { pagination: number } } };
+    }).site.siteMetadata.blogList.pagination;
+  }
 };
 
 export interface PageContext {
   slug: string;
-  previous: Post;
-  next: Post;
+  previous: Post | null;
+  next: Post | null;
+}
+
+export interface BlogListContext {
+  pageIndex: number;
+  mdStartIndex: number;
+  mdCount: number;
+  wpStartIndex: number;
+  wpCount: number;
 }
 
 async function getPostsFromMarkdown(graphql: any): Promise<Post[]> {
@@ -73,11 +158,15 @@ async function getPostsFromMarkdown(graphql: any): Promise<Post[]> {
   );
 
   const posts = markdownResults.data.allMarkdownRemark.edges;
-  return posts.map(post => ({
-    slug: post.node.fields.slug,
-    title: post.node.frontmatter.title,
-    date: new Date(post.node.frontmatter.date),
-  }));
+  return posts
+    .map(post => ({
+      slug: post.node.fields.slug,
+      title: post.node.frontmatter.title,
+      date: new Date(post.node.frontmatter.date),
+      source: 'md' as const,
+    }))
+    .sort((post1, post2) => post2.date.getTime() - post1.date.getTime())
+    .map((post, index) => ({ ...post, index }));
 }
 
 async function getPostsFromWordPress(graphql: any): Promise<Post[]> {
@@ -110,10 +199,12 @@ async function getPostsFromWordPress(graphql: any): Promise<Post[]> {
   );
 
   const posts = wordpressResult.data.allWordpressPost.edges;
-  return posts.map(post => ({
+  return posts.map((post, index) => ({
     slug: post.node.slug,
     title: post.node.title,
     date: new Date(post.node.date),
+    index,
+    source: 'wp',
   }));
 }
 
@@ -121,4 +212,6 @@ interface Post {
   slug: string;
   title: string;
   date: Date;
+  index: number;
+  source: 'md' | 'wp';
 }
