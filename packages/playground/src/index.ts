bd300527e65540f96cc1aeac0e57277bdf86ea42
deruideng <moonface.ddr@gmail.com>
2020-05-14 18:20:21 +0800

    Playground: Test code

--- packages/playground/src/index.ts
+++ packages/playground/src/index.ts
@@ -1,3 +1,17 @@
-export function playground(){
-  console.log('This is a playground.')
+export function playground() {
+  console.log('This is a playground.');
+  test()
+    .then(() => console.log('ok'))
+    .catch(e => console.log(`Exception:`, e));
+}
+
+async function test() {
+  const result = await willReject();
+  console.log('Ok, working fine.', result);
+}
+
+function willReject() {
+  return new Promise((resolve, reject) => {
+    reject('Rejected.');
+  });
 }
