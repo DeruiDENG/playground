ff6e5742d51754bab9f71e07f40c803e5777f7d9
Derui DENG <moonface.ddr@gmail.com>
2020-05-14 22:49:12 +0800

    LC-136 Use XOR to implement.

--- packages/leetcode/src/q136/answer.js
+++ packages/leetcode/src/q136/answer.js
@@ -5,20 +5,12 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-  /**
-   * @type {Map<number, number>}
-   */
-  const map = new Map();
+  let result = 0;
   for (const num of nums) {
-    const count = map.get(num);
-    if (count) {
-      map.set(num, count + 1);
-    } else {
-      map.set(num, 1);
-    }
+    result = result ^ num;
   }
 
-  return Array.from(map.entries()).find(entry => entry[1] === 1)[0];
+  return result;
 };
 
 export { singleNumber };
